7f1c1dcc5b9074d9664b3c1218bc7aa4ae481d9a
SpenserCai <spenser@ygens.cn>
2022-01-20 10:47:13 +0800

    完成 8.1.1.1-8.1.1.5以及8.1.1.10的功能

--- sangfor_af_sdk/Object/CpuUsages.py
+++ sangfor_af_sdk/Object/CpuUsages.py
@@ -3,25 +3,13 @@ Author: 饕餮
 Date: 2022-01-20 09:40:09
 version: 
 LastEditors: 饕餮
-LastEditTime: 2022-01-20 09:49:57
+LastEditTime: 2022-01-20 10:28:08
 Description: cpu使用率变化曲线数据
 '''
 from typing import List
-from sangfor_af_sdk.Common.BaseObject import BaseObject
+
 from sangfor_af_sdk.Common.BaseResponseObject import BaseResponseObejct
-
-class Usage(BaseObject):
-    def __init__(self,jsonData):
-        self.ObjectData = jsonData
-
-    @property
-    def Usage(self):
-        return self.TryGetValue("usage")
-
-    @property
-    def Time(self):
-        return self.TryGetValue("time")
-    
+from sangfor_af_sdk.Object.StatusCenterBase import Usage
 
 class CpuUsages(BaseResponseObejct):
 
